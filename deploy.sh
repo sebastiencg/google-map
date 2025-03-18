73d01c79f6dab8651fc7b2d96a78f1f69354f113
sebastien <miantamag@gmail.com>
2025-03-18 16:05:28 +0100

    projet google map

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "🚀 Déploiement Node.js + PM2 en cours..."
+
+# Variables
+PROJECT_DIR="/var/www/google-map"
+USER="www-data" # ou ton user système si nécessaire
+
+cd $PROJECT_DIR || exit
+
+echo "🔄 Pull Git..."
+git pull
+
+echo "🛑 Stop PM2"
+pm2 stop all
+
+echo "📦 Install des dépendances..."
+npm install
+
+echo "⚙️ Build de l'application..."
+npm run build
+
+echo "🚀 Redémarrage PM2"
+pm2 start all
+
+echo "📊 Status PM2 :"
+pm2 status
+
+echo "📝 Logs en cours (ctrl + c pour sortir) :"
+pm2 logs
